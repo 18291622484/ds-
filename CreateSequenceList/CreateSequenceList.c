b46eeb881be04cfc14e4c9d7339c71a525701c52
18291622484 <18291622484@163.com>
2020-12-04 10:50:42 +0800

    创建顺序表 顺序表的各个接口均已实现，是个比较完善的顺序表

--- CreateSequenceList/CreateSequenceList.c
+++ CreateSequenceList/CreateSequenceList.c
@@ -4,9 +4,19 @@
 #include"SQList.h"
 
 //顺序表初始化
-void initSeqList(SeqList* s1) {
+void SeqListInit(SeqList* s1) {
 	s1->_date = NULL;
 	s1->_size = s1->_capacity = 0;
+}
+
+//顺序表销毁
+void SeqListDestroy(SeqList* s1) {
+	//参数合法性检验,是空就不用释放了
+	if (s1 == NULL||s1->_date == NULL) {
+		return;
+	}
+	free(s1->_date);
+	s1->_date = NULL;
 }
 
 //检查顺序表是否已满
@@ -19,9 +29,9 @@ void CheckCapacity(SeqList* s1) {
 		//由于一开始容量为0，不好进行相乘运算，所以先增加1
 		s1->_capacity = s1->_capacity == 0 ? 1 : s1->_capacity;
 		//每次以二倍的容量增加
-		SequenceList* temp = (SequenceList*)malloc(sizeof(SequenceList) * 2 * s1->_capacity);
+		SeqListType* temp = (SeqListType*)malloc(sizeof(SeqListType) * 2 * s1->_capacity);
 		//使用memcpy函数来进行将旧空间的数据拷贝到新空间
-		memcpy(temp, s1->_date, sizeof(SequenceList) * s1->_size);
+		memcpy(temp, s1->_date, sizeof(SeqListType) * s1->_size);
 		//释放掉旧空间
 		free(s1->_date);
 		//更新变量
@@ -32,7 +42,7 @@ void CheckCapacity(SeqList* s1) {
 }
 
 //顺序表尾插
-void SeqListPushBack(SeqList* s1, SequenceList num) {
+void SeqListPushBack(SeqList* s1, SeqListType num) {
 	//参数合法性检验
 	if (s1 == NULL) {
 		return;
@@ -40,30 +50,137 @@ void SeqListPushBack(SeqList* s1, SequenceList num) {
 	//在插入数据之前应该检查是否满了
 	CheckCapacity(s1);
 	s1->_date[s1->_size] = num;
-	s1->_size++;
+	++s1->_size;
+}
+
+//顺序表尾删 
+void SeqListPopBack(SeqList* s1) {
+	//参数合法性检验
+	if (s1 == NULL||s1->_size == 0) {
+		return;
+	}
+	--s1->_size;
+}
+
+//顺序表头插
+void SeqListPushFront(SeqList* s1, SeqListType val) {
+	//参数合法性检验
+	if (s1 == NULL) {
+		return;
+	}
+	CheckCapacity(s1);
+	int end = s1->_size;
+	while (end > 0) {
+		s1->_date[end] = s1->_date[end - 1];
+		--end;
+	}
+	s1->_date[0] = val;
+	++s1->_size;
+}
+
+//顺序表头删
+void SeqListPopFront(SeqList* s1) {
+	//参数合法性检验
+	if (s1 == NULL||s1->_size == 0) {
+		return;
+	}
+	int start = 1;
+	while (start < s1->_size) {
+		s1->_date[start - 1] = s1->_date[start];
+		++start;
+	}
+	--s1->_size;
+}
+
+//顺序表任意插
+void SeqListInsert(SeqList* s1, int pos, SeqListType val) {
+	//参数合法性检验
+	if (s1 == NULL) {
+		return;
+	}
+	CheckCapacity(s1);
+	int end = s1->_size;
+	while (end > pos) {
+		s1->_date[end] = s1->_date[end - 1];
+		--end;
+	}
+	s1->_date[pos] = val;
+	++s1->_size;
+}
+
+//顺序表任意删
+void SeqListErase(SeqList* s1, int pos) {
+	//参数合法性检验
+	if (s1 == NULL || s1->_size == 0) {
+		return;
+	}
+	++pos;
+	while (pos < s1->_size) {
+		s1->_date[pos - 1] = s1->_date[pos];
+		++pos;
+	}
+	--s1->_size;
+}
+
+//打印顺序表
+void SeqListPrint(SeqList* s1) {
+	//参数合法性检验
+	if (s1 == NULL || s1->_size == 0) {
+		return;
+	}
+	for (int i = 0; i < s1->_size; ++i) {
+		printf("%d ", s1->_date[i]);
+	}printf("\n");
+}
+
+//顺序表判空
+int SeqListEmpty(SeqList* s1) {
+	if (s1 == NULL || s1->_size == 0) {
+		return 0;
+	}
+	return 1;
+}
+
+//顺序表大小
+int SeqListSize(SeqList* s1) {
+	if (s1 == NULL) {
+		return 0;
+	}
+	return s1->_size;
+}
+
+//顺序表查找下标,这个模块代码不完善
+//因为一个顺序表中重复的数字很多,所以我们应该返回一个数组，里面包含的val出现的所有位置
+//但是实现起来较麻烦，所以不在此展示
+int SeqListFindIdx(SeqList* s1, SeqListType val) {
+	if (s1 == NULL||s1->_size == 0) {
+		return -1;
+	}
+	for (int i = 0; i < s1->_size; ++i) {
+		if (s1->_date[i] == val) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+//顺序表下标对应元素
+SeqListType SeqListGetIdx(SeqList* s1, int pos) {
+	//此模块无法进行判空，因为如果为空，那么该返回什么值呢？
+	//返回的值都有可能是顺序表中的某一个值，所以此模块默认已经进行判空
+	return s1->_date[pos];
 }
 
 int main() {
 	SeqList s1;
-	initSeqList(&s1);
+	SeqListInit(&s1);
 	SeqListPushBack(&s1, 0);
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 9);
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 1);
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 4); 
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 0);
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 9);
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 1);
-	printf("%d ", s1._capacity);
 	SeqListPushBack(&s1, 4); 
-	printf("%d \n", s1._capacity);
-	for (int i = 0; i < s1._size; i++) {
-		printf("%d\n", s1._date[i]);
-	}
 	return 0;
 }
--- CreateSequenceList/SQList.h
+++ CreateSequenceList/SQList.h
@@ -1,12 +1,12 @@
 #pragma once
 
 //使用这种方式来重命名数据类型，这样可以很方便的修改后续数据的数据类型，相当于#define的作用
-typedef int SequenceList;
+typedef int SeqListType;
 
 //动态创建顺序表
 typedef struct SeqList {
 	//使用指针指向一块动态开辟的内存
-	SequenceList* _date;
+	SeqListType* _date;
 	//表示顺序表中的有效数据
 	size_t _size;
 	//表示顺序表的最大容量
@@ -15,8 +15,30 @@ typedef struct SeqList {
 
 //包含所有函数的声明
 //顺序表初始化
-void initSeqList(SeqList* s1);
+void SeqListInit(SeqList* s1);
+//顺序表销毁
+void SeqListDestroy(SeqList* s1);
 //检查顺序表是否已满
 void CheckCapacity(SeqList* s1);
 //顺序表尾插
-void SeqListPushBack(SeqList* s1, SequenceList num);
+void SeqListPushBack(SeqList* s1, SeqListType val);
+//顺序表尾删 
+void SeqListPopBack(SeqList* s1);
+//顺序表头插
+void SeqListPushFront(SeqList* s1, SeqListType val);
+//顺序表头删
+void SeqListPopFront(SeqList* s1);
+//顺序表任意插
+void SeqListInsert(SeqList* s1, int pos, SeqListType val);
+//顺序表任意删
+void SeqListErase(SeqList* s1, int pos);
+//打印顺序表
+void SeqListPrint(SeqList* s1);
+//顺序表判空
+int SeqListEmpty(SeqList* s1);
+//顺序表大小
+int SeqListSize(SeqList* s1);
+//顺序表查找下标
+int SeqListFindIdx(SeqList* s1, SeqListType val);
+//顺序表下标对应元素
+SeqListType SeqListGetIdx(SeqList* s1, int pos);
